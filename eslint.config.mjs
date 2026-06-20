82f2909a66cbd62dbf58d31bc5d01dbb7d227f6c
AlamCs32 <skmdmehfoozalam@gmail.com>
2026-06-20 13:53:41 +0530

    feature: add auth

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+    js.configs.recommended,
+
+    ...tseslint.configs.recommended,
+
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+        },
+
+        rules: {
+            "no-console": "warn",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                },
+            ],
+        },
+    },
+];
